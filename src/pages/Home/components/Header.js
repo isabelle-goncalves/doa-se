1cb4deaa92f16f82182ad1c006474a1f926b2d9f
Isabelle Gonçalves <isabelle.g.arcanjo@gmail.com>
2023-04-18 21:10:24 -0300

    1 tela home começada

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Header() {
+    return (
+<header className="header">
+<div className="toolbar">
+    <div className="lado_esquerdo_menu">
+        <span>Doa-se logo</span>    
+    </div>
+
+    <div className="lado_direito_menu">
+        <button>Novo Post</button>
+        <span>img1</span>
+        <span>img2</span>
+    </div>
+</div>
+</header>
+
+);
+}
+
+export default Header;
